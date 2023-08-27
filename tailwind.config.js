bdfa7ab480c04bda609f674ba9c087beec021dde
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-08-28 00:43:06 +0600

    Footer added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		daisyui: {
+			themes: ['light', 'dark'],
+		},
+		fontFamily: {
+			Zillo: ['Zilla Slab', 'serif'],
+		},
 	},
 	plugins: [require('daisyui')],
 };
